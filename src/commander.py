18c0b6be35872ecde55658c6e389ab90726130db
PePacheco <pedro_pacheco98@outlook.com>
2024-12-01 18:37:45 -0300

    Code enhancements

--- src/commander.py
+++ src/commander.py
@@ -9,11 +9,18 @@ self_ip = os.environ.get("HOST_IP")
 self_node_type = os.environ.get("NODE_TYPE")
 self_node_id = os.environ.get("NODE_ID")
 
+hosts_and_ports_map = {
+    "acceptor": [("acceptor0", 5000), ("acceptor1", 5001), ("acceptor2", 5002)],
+    # "leader": [("leader0", 5100), ("leader1", 5101)],
+    "leader": [("leader0", 5100)],
+    "replica": [("replica0", 5200), ("replica1", 5201)],
+}
+
 class Commander(Process):
     def __init__(self, env, id, leader, acceptors, replicas, ballot_number, slot_number, command, host, port):
         Process.__init__(self, env, id, host, port)
         self.leader = leader
-        self.acceptors = acceptors
+        self.acceptors = hosts_and_ports_map["acceptor"]
         self.replicas = replicas
         self.ballot_number = ballot_number
         self.slot_number = slot_number
@@ -40,6 +47,7 @@ class Commander(Process):
             if isinstance(msg, P2bMessage):
                 if self.ballot_number == msg.ballot_number and msg.src in waitfor:
                     waitfor.remove(msg.src)
+                    print "waitfor", waitfor
                     if len(waitfor) < float(len(self.acceptors))/2:
                         message = DecisionMessage(self.id, self.slot_number, self.command)
                         self.env.broadcast_message_to_leaders(message)
